7b859987eb39e90a073ef6d27da2439825e1d44e
TrieuHuy <trieuxuanhuytq3105@gmail.com>
2025-12-10 16:58:19 +0700

    dele module business

--- business-service/src/main/java/com/ecommerce/post_service/repository/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ecommerce.post_service.repository;
-
-import com.ecommerce.post_service.domain.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-    List<Post> findByUserId(Long userId);
-}
